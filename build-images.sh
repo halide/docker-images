344637d0dd57b3d95061edf98653f0cd097f9b21
Alex Reinking <areinking@adobe.com>
2024-08-29 16:50:26 -0400

    only generate x86_64 packages

--- build-images.sh
+++ build-images.sh
@@ -4,16 +4,17 @@ set -eo pipefail
 
 [[ "$1" == "" ]] && echo "You must specify the LLVM version as an argument, e.g. 17.0.6" && exit
 
-LLVM_TAG="$1"
+LLVM_VERSION="$1"
+LLVM_VERSION_MAJOR="${LLVM_VERSION%%.*}"
 
-build_arch () {
-  ARCH="$1"
+build_image () {
+  IMAGE="$1"; shift
   docker build \
-    --tag "ghcr.io/halide/manylinux_2_28_$ARCH-llvm:$LLVM_TAG" \
-    --build-arg "LLVM_TAG=llvmorg-$LLVM_TAG" \
-    --build-arg "ARCH=$ARCH" \
-    .
+    --tag "ghcr.io/halide/$IMAGE:$LLVM_VERSION" \
+    --tag "ghcr.io/halide/$IMAGE:$LLVM_VERSION_MAJOR" \
+    --build-arg "LLVM_VERSION=$LLVM_VERSION" \
+    "$@" \
+    "$IMAGE"
 }
 
-build_arch aarch64
-build_arch x86_64
+build_image manylinux_2_28_x86_64-llvm
